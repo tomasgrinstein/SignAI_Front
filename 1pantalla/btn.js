f9d8a40a757807c782d9285197b742bcd7bde802
totogrinstein <147621859+totogrinstein@users.noreply.github.com>
2024-08-15 15:24:30 -0300

    js

--- 1pantalla/btn.js
+++ 1pantalla/btn.js
@@ -1,3 +1,19 @@
 document.getElementById('bt').addEventListener('click', function() {
     window.location.href = '../2pantalla/index.html'; // Ruta al archivo HTML al que quieres redirigir
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const panelHeader = document.getElementById('fotito');
+    const panelContent = document.getElementById('dropdown');
+
+    panelHeader.addEventListener('click', () => {
+        // Alternar la clase 'show' para mostrar/ocultar el contenido
+        panelContent.classList.toggle('mostrar');
+    });
+
+    // Cerrar el panel si se hace clic fuera de él
+    document.addEventListener('click', (event) => {
+        if (!panelHeader.contains(event.target) && !panelContent.contains(event.target)) {
+            panelContent.classList.remove('mostrar');
+        }
+    });
+});
